0413c033b5c440140c8770d80ee1ef0089f28f94
babubv <Vignesh.Babu@rsa.com>
2019-09-08 13:37:07 +0530

    configFailurePolicy continue test case

---
+++ src/test/java/Tests.java
@@ -0,0 +1,27 @@
+import org.testng.annotations.Test;
+
+public class Tests {
+    @Test
+    public void t1()
+    {
+        System.out.println("t1");
+    }
+
+    @Test
+    public void t2()
+    {
+        System.out.println("t2");
+    }
+
+    @Test
+    public void t3()
+    {
+        System.out.println("t3");
+    }
+
+    @Test
+    public void t4()
+    {
+        System.out.println("t4");
+    }
+}
